1ef2a3bca23e7203efbeec1bf171f2cc3f97ea91
Cesar-Hernandez-1 <cesarhernandez@Knights.ucf.edu>
2021-10-01 00:16:12 -0400

    Psudocode for exercise30 Date: 10/1/2021

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,14 @@
+package baseline;
+
+public class Solution30 {
+    //create multiplicationTable class instance to use method
+    multiplicationTable app = new multiplicationTable();
+
+    public static void main(String[] args) {
+        //use makeTable method to make a multiplication table
+        //Store table in String variable for output
+        String output = app.makeTable();
+
+        //output table to user.
+    }
+}
---
+++ exercise30/src/main/java/baseline/multiplicationTable.java
@@ -0,0 +1,17 @@
+package baseline;
+
+public class multiplicationTable {
+    //create String variable to build multiplication table in
+
+    public String makeTable(){
+        //use for loop to iterate through the integers 1 to 12
+        //format table by appending spaces for each outer for loop iteration.
+
+        //      inner for loop thorough int 1 to 12
+        //      append multiplication of outer for loop variable i and inner for loop variable j
+
+        //      append spaces based on result for formatting correctness
+
+        //return table
+    }
+}
