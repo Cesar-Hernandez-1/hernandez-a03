54e3a5e7040481e3b3d6d35d9ceebbcc2413bb26
Cesar-Hernandez-1 <cesarhernandez@Knights.ucf.edu>
2021-10-01 00:23:02 -0400

    complete exercise30 Date: 10/1/2021

--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -1,14 +1,16 @@
 package baseline;
 
 public class Solution30 {
-    //create multiplicationTable class instance to use method
-    multiplicationTable app = new multiplicationTable();
 
     public static void main(String[] args) {
+        //create multiplicationTable class instance to use method
+        multiplicationTable app = new multiplicationTable();
+
         //use makeTable method to make a multiplication table
         //Store table in String variable for output
         String output = app.makeTable();
 
         //output table to user.
+        System.out.print(output);
     }
 }
--- exercise30/src/main/java/baseline/multiplicationTable.java
+++ exercise30/src/main/java/baseline/multiplicationTable.java
@@ -2,16 +2,39 @@ package baseline;
 
 public class multiplicationTable {
     //create String variable to build multiplication table in
+    private final StringBuilder table = new StringBuilder();
 
     public String makeTable(){
         //use for loop to iterate through the integers 1 to 12
-        //format table by appending spaces for each outer for loop iteration.
+        for(int i = 1; i < 13; i++){
+            //format table by appending spaces for each outer for loop iteration.
+            if(i < 10){
+                table.append("    ");
+            }else{
+                table.append("   ");
+            }
 
-        //      inner for loop thorough int 1 to 12
-        //      append multiplication of outer for loop variable i and inner for loop variable j
+            //inner for loop thorough int 1 to 12
+            for(int j = 1; j < 13; j++){
 
-        //      append spaces based on result for formatting correctness
+                //append multiplication of outer for loop variable i and inner for loop variable j
+                table.append(i*j);
+                if(j == 12){
+                    table.append("\n");
+                }
 
+                //append spaces based on result for formatting correctness
+                if(i*j < 10 && (i*(j+1)) < 10 && j != 12){
+                    table.append("    ");
+                }else if((i*j > 10 || (i*(j+1)) >= 10) && i*j < 100 && (i*(j+1)) < 100 && j != 12){
+                    table.append("   ");
+                }else if(j != 12){
+                    table.append("  ");
+                }
+            }
+        }
         //return table
+        table.replace(table.length() - 1, table.length(),"");
+        return table.toString();
     }
 }
